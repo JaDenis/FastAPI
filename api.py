0371ef7684f20339831db84bf2f5a2172b0ce75e
lalala <lalala@gmail.com>
2024-01-14 20:29:18 +0300

    Finally updated css

--- api.py
+++ api.py
@@ -11,15 +11,20 @@ async def index():
 async def create_agent(agent: str):
     return {"message": agent}
 
-@app.get("/docs2", include_in_schema=False)
-async def custom_swagger_ui_html_github():
-    print("lol kek")
+@app.get("/api", include_in_schema=False)
+async def dark_swagger_ui():
     return get_swagger_ui_html(
         openapi_url=app.openapi_url,
         title=f"{app.title} - Fast UI", 
         swagger_ui_parameters={"syntaxHighlight.theme": "arta", "defaultModelsExpandDepth": -1, "persistAuthorization": True}, # monokai arta
-        swagger_css_url = "https://cdn.jsdelivr.net/gh/JaDenis/Fastapi-Swagger-UI-Dark/assets/swagger_ui_dark.min.css"
+        swagger_css_url = "https://cdn.jsdelivr.net/gh/Madrobotz/Fastapi-Swagger-UI-Dark/assets/swagger_ui_dark.min.css"
     )
+
+    # For more swagger_ui_parameters visit: https://springdoc.org/properties.html#_swagger_ui_properties
+
+    # TODO: Edit css for components and host css on a new github account to fork from there
     # loading animation
     # button.opblock-control-arrow
     # .copy-to-clipboard button
+    # select.content-type
+    # input
