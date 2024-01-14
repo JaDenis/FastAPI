b6b88f23bc4b4cb11513ea358f7916bff50c98ee
lalala <lalala@gmail.com>
2024-01-14 16:55:21 +0300

    Add Swagger Dark Theme

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
 
-app = FastAPI()
+app = FastAPI(swagger_ui_parameters={"swagger_css_url"})
 
 @app.get("/")
 async def index():
@@ -10,3 +11,10 @@ async def index():
 async def create_agent(agent: str):
     return {"message": agent}
 
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html_github():
+    return get_swagger_ui_html(
+        openapi_url = app.openapi_url,
+        title=f"{app.title} - Swagger UI",
+        swagger_css_url = "https://raw.githubusercontent.com/JaDenis/Fastapi-Swagger-UI-Dark/main/assets/swagger_ui_dark.min.css"
+    )
