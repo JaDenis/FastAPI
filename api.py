9af3da917f60f2a03bac2db2a8ca25be694cb187
lalala <lalala@gmail.com>
2024-01-14 17:04:02 +0300

    Fix try2

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.openapi.docs import get_swagger_ui_html
 
-app = FastAPI(swagger_ui_parameters={"swagger_css_url"})
+app = FastAPI()
 
 @app.get("/")
 async def index():
@@ -15,5 +15,6 @@ async def create_agent(agent: str):
 async def custom_swagger_ui_html_github():
     return get_swagger_ui_html(
         openapi_url = app.openapi_url,
-        swagger_css_url = "https://raw.githubusercontent.com/JaDenis/Fastapi-Swagger-UI-Dark/main/assets/swagger_ui_dark.css"
+        title=f"{app.title} - Swagger UI",
+        swagger_css_url = "https://raw.githubusercontent.com/JaDenis/Fastapi-Swagger-UI-Dark/main/assets/swagger_ui_dark.min.css"
     )
